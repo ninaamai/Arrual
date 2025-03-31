fae0f16be8c32f0a617c17650ff5b3c4b0e30fea
Nina <ninaamai@mail.ru>
2025-03-31 05:54:54 +0300

    feat: add files
    feat: add files

---
+++ Controllers/AutorizationController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Arrual.Controllers
+{
+    public class AutorizationController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
